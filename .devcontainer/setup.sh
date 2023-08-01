02d5878baf413fa4a6dec93bb5d5d09a12534b12
Craig Peters <craiglpeters@github.com>
2023-08-01 17:49:42 +0000

    refactor setup.sh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -16,7 +16,5 @@
 
 set -eux
 
-python3 -m pip install --user --upgrade --no-cache-dir pyyaml
-
-echo "Cloning kubernetes/kubectl" && mkdir -p /go/src/sigs.k8s.io && cd /go/src/sigs.k8s.io && git clone --depth=1 https://github.com/kubernetes/kubectl
-echo "Cloning golang/term" && cd /go/src && git clone --depth=1 https://github.com/golang/term.git
+# Copies over welcome message
+cp .devcontainer/welcome-message.txt /usr/local/etc/vscode-dev-containers/first-run-notice.txt
